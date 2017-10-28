4a09a6ebf1766291b50ca529e8c55c1703dfe4c1
Nathan Manceaux-Panot <cykelero@gmail.com>
2017-10-28 21:02:09 +0200

    Initial commit!

---
+++ source/tasklemon.js
@@ -0,0 +1,39 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+const os = require('os');
+const path = require('path');
+
+const injectedModuleNames = ['cli'];
+const baseInjectedModulePath = path.join(__dirname, 'injected-modules');
+
+const workingDirectory = process.cwd();
+const scriptName = process.argv[2];
+const scriptPath = path.join(workingDirectory, scriptName);
+
+let preparedScriptPath;
+
+const scriptArguments = process.argv.slice(2);
+
+// Prepare execution stage
+// // Create stage folder
+const stagePath = fs.mkdtempSync(os.tmpdir() + path.sep);
+preparedScriptPath = path.join(stagePath, scriptName);
+
+// // Generate script
+let preparedScriptContent = '';
+
+preparedScriptContent += 'const ';
+injectedModuleNames.forEach((injectedModuleName, index) => {
+	const injectedModulePath = path.join(baseInjectedModulePath, injectedModuleName);
+	if (index > 0) preparedScriptContent += ',';
+	preparedScriptContent += `${injectedModuleName} = require('${injectedModulePath}')`;
+});
+preparedScriptContent += ';';
+
+preparedScriptContent += fs.readFileSync(scriptPath);
+
+fs.writeFileSync(preparedScriptPath, preparedScriptContent);
+
+// Execute script
+require(preparedScriptPath);
