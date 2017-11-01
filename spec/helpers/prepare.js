2ba65f15f2b39dfd20caa96380bb2fab55286e29
Nathan Manceaux-Panot <cykelero@gmail.com>
2017-11-01 20:48:08 +0100

    Add Jasmine for testing

---
+++ spec/helpers/prepare.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const os = require('os');
+const path = require('path');
+
+beforeEach(function() {
+	this.getTestEnv = function() {
+		return {
+			path: fs.mkdtempSync(os.tmpdir() + path.sep),
+			createFile: function(filePath) {
+				const completeFilePath = this.pathFor(filePath);
+				fs.closeSync(fs.openSync(completeFilePath, 'w'));
+				return completeFilePath;
+			},
+			createFolder: function(folderPath) {
+				const completeFolderPath = this.pathFor(folderPath);
+				fs.mkdirSync(completeFolderPath);
+				return path.join(completeFolderPath, path.sep);
+			},
+			pathFor: function(itemPath) {
+				return path.join(this.path, itemPath);
+			}
+		};
+	};
+});
