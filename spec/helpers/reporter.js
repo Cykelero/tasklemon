b41d8b928a771044bdc032a5eb9cd9c074c66e78
Nathan Manceaux-Panot <cykelero@gmail.com>
2017-12-10 19:01:05 +0100

    Add jasmine-spec-reporter

---
+++ spec/helpers/reporter.js
@@ -0,0 +1,8 @@
+const SpecReporter = require('jasmine-spec-reporter').SpecReporter;
+
+jasmine.getEnv().clearReporters();
+jasmine.getEnv().addReporter(new SpecReporter({
+  spec: {
+    displayPending: true
+  }
+}));
