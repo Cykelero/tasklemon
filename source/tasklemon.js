96a63acec62aa930bfd26cf823c970c4c61602be
Nathan Manceaux-Panot <cykelero@gmail.com>
2019-03-25 13:13:47 +0100

    Validate Tasklemon arguments

--- source/tasklemon.js
+++ source/tasklemon.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 const ScriptRunner = require('./ScriptRunner');
+const Tools = require('./Tools');
 
 const validLemonArguments = [];
 const validNodeArguments = ['--inspect', '--inspect-brk'];
@@ -11,6 +12,8 @@ function parseProgramArguments(argumentList) {
 	
 	const ourArguments = rawArguments.slice(0, scriptPathIndex);
 	
+	exitIfContainsInvalidArguments(ourArguments);
+	
 	const lemonArguments = ourArguments.filter(arg => validLemonArguments.includes(arg));
 	const nodeArguments = ourArguments.filter(arg => validNodeArguments.includes(arg));
 	
@@ -20,6 +23,27 @@ function parseProgramArguments(argumentList) {
 		nodeArguments,
 		scriptArguments: rawArguments.slice(scriptPathIndex + 1)
 	};
+}
+
+function exitIfContainsInvalidArguments(args) {
+	const invalidArguments = args.filter(arg =>
+		!validLemonArguments.includes(arg)
+		&& !validNodeArguments.includes(arg)
+	);
+	
+	
+	if (invalidArguments.length > 0) {
+		let message;
+		
+		const s = invalidArguments.length > 1 ? 's' : '';
+		message = `Argument error: invalid Tasklemon argument${s}: `;
+		
+		invalidArguments.forEach((invalidArg, index) => {
+			message += (index === 0 ? '' : ', ') + `“${invalidArg}”`;
+		});
+	
+		Tools.exitWithError(message);
+	}
 }
 
 // Run
