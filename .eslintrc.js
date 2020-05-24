680f68818444b01ce7c372231e29e1c026c0b419
Nathan Manceaux-Panot <cykelero@gmail.com>
2020-05-24 20:53:02 +0200

    Meta: Add ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,43 @@
+module.exports = {
+    root: true,
+    env: {
+        node: true,
+        es6: true
+    },
+    parserOptions: {
+        ecmaVersion: 11,
+        sourceType: 'module'
+    },
+    extends: 'eslint:recommended',
+    rules: {
+        'dot-location': ['error', 'property'],
+        'no-multi-spaces': 'error',
+        'no-multi-str': 'error',
+        'grouped-accessor-pairs': ['error', 'getBeforeSet'],
+        
+        'consistent-return': 'error',
+        'no-var': 'error',
+        
+        'accessor-pairs': 'error',
+        'default-param-last': 'error',
+        'require-atomic-updates': 'error',
+        'array-callback-return': 'error',
+        'no-return-assign': 'error',
+        'no-self-compare': 'error',
+        'no-useless-backreference': 'error',
+        'no-sequences': 'error',
+        'no-unused-expressions': 'error',
+        'no-empty': ['error', {allowEmptyCatch: true}],
+        
+        'no-implied-eval': 'error',
+        'no-warning-comments': 'error'
+    },
+    overrides: [
+        {
+            files: ['spec/**/*.js'],
+            env: {
+                jasmine: true
+            }
+        }
+    ]
+};
