be6a4672bbce5a70350176aa640f98cf35596415
Nathan Manceaux-Panot <cykelero@gmail.com>
2022-01-14 17:38:22 +0100

    Add 0.4 version bin, fix existing file

--- source/version-bin/tasklemon-v0.4.js
+++ source/version-bin/tasklemon-v0.4.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
 /* Exposed on the PATH. Allows specifying Tasklemon version in a script's shebang. */
 
-process.env.TASKLEMON_RUNTIME_VERSION = '0.3';
+process.env.TASKLEMON_RUNTIME_VERSION = '0.4';
 require('../tasklemon');
